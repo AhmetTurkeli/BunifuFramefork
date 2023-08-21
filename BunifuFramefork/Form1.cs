66f83ed30228b54b70e1dcc86c1ca525c69cd2d9
AhmetTurkeli <ahmetyusufturkeli@gmail.com>
2023-08-21 23:31:40 +0300

    Proje dosyası ekle.

---
+++ BunifuFramefork/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BunifuFramefork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuPictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        bool move;
+        int mouse_x;
+        int mouse_y;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move == true)
+            {
+                this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mouse_x = e.X;
+            mouse_y = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            move = false;
+        }
+    }
+}
